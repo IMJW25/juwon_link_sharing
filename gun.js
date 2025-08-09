35e7386b0b96626c5850bdc9bbdda0a2a4fd77a7
IMJW725 <183794955+IMJW25@users.noreply.github.com>
2025-08-09 21:54:39 +0900

    Update gun.js
    gun 전체 주석화

--- gun.js
+++ gun.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const http = require('http');
-const Gun = require('gun');
+// const express = require('express');
+// const http = require('http');
+// const Gun = require('gun');
 
-const app = express();
+// const app = express();
 
-app.use(require('cors')());
-app.use(Gun.serve);
+// app.use(require('cors')());
+// app.use(Gun.serve);
 
-const server = http.createServer(app);
-Gun({ web: server, file: 'data' });
+// const server = http.createServer(app);
+// Gun({ web: server, file: 'data' });
 
-server.listen(8765, () => {
-  console.log('Gun relay server listening on http://localhost:8765');
-});
+// server.listen(8765, () => {
+//   console.log('Gun relay server listening on http://localhost:8765');
+
+// });
